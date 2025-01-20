3d86c4ddccca30ee956b7417ac96d4bc21300529
Chandrashekar M <mchandrashekar983@gmail.com>
2025-01-20 23:05:10 +0530

    changd

--- RemoveD.cs
+++ RemoveD.cs
@@ -7,14 +7,24 @@ using System.Threading.Tasks;
 namespace Cprograms
 {
     public class RemoveD    
-    {                //using linq chandrashekar
+    {                
         public static void Main()
         {
+            //hi
             string input = "swamiiii";
             string result = new string(input.ToCharArray().Distinct().ToArray());
             Console.WriteLine(result);
         }
     }
 }
+/*1. input.ToCharArray(): This converts the input string into a character array.
+
+2.Distinct(): This method is part of the LINQ (Language Integrated Query) library in C#.
+It removes duplicate elements from a sequence, in this case, the character array.
+
+3. ToArray(): This converts the result of the Distinct() method back into an array.
+
+4. new string(...): This creates a new string from the character array*/
+
 
 
